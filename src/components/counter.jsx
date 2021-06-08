e657b5931b210f24164355c970087e9f14dec5b3
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-08 11:52:35 +0300

    conditiolar rendering, && use

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,60 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    // locali componento busena
-    state = {
-        count: 1,
-        imgUrl: 'https://placeimg.com/200/200/tech'
-    }
+  // locali componento busena
+  state = {
+    count: 1,
+    imgUrl: 'https://placeimg.com/200/200/tech',
+    colors: [],
+  };
 
-    h2ElStyles = {
-        background: 'tomato',
-        color: 'snow',
-        textAlign: 'center'
-    }
+  h2ElStyles = {
+    background: 'tomato',
+    color: 'snow',
+    textAlign: 'center',
+  };
 
-    render() {
-        return (
-            <div className="container mt-4" >
-                <h2 style={{fontSize: '2em'}} >I am React counter component</h2>
-                <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
-                <button className="btn btn-warning" > Press me</button>
-                <div className="mt-5" >
-                    <img src={this.state.imgUrl} alt="tech" />
-                </div>
-            </div>
-        );
-    }
+  renderColors() {
+    // if (this.state.colors.length === 0)
+    //   return <p className="alert alert-warning">There are no colors left</p>;
+      
+    return this.state.colors.map((color) => (
+      <li key={color} style={{ background: color }} className="list-group-item">
+        {color}
+      </li>
+    ));
+  }
 
-    getBadgeClasses() {
-        let badgeClasses = 'badge mr-3 badge-';
-        badgeClasses += this.state.count === 0 ? 'danger' : 'info';
-        return badgeClasses;
-    }
+  render() {
+    return (
+      <div className="container mt-4">
+        <h2 style={{ fontSize: '2em' }}>I am React counter component</h2>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-warning"> Press me</button>
 
-    formatCount() {
-        const {count} = this.state;
-        return count === 0 ? "Out of stock" : count;
-    }
+        <ul className="list-group mt-4">
+            {/* Jei kaire salyga true grazinam kas po desine && ... */}
+            {this.state.colors.length === 0 && <p className="alert alert-warning">There are no colors left</p>}
+            {this.renderColors()}
+            </ul>
+
+        <div className="mt-5">
+          <img src={this.state.imgUrl} alt="tech" />
+        </div>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let badgeClasses = 'badge mr-3 badge-';
+    badgeClasses += this.state.count === 0 ? 'danger' : 'info';
+    return badgeClasses;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Out of stock' : count;
+  }
 }
 
 export default Counter;
