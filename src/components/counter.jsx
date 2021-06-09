7f9b903e9d3131e29960a560ad272f78d0c31470
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-09 09:46:58 +0300

    event this bind + arrow solutions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,25 +14,38 @@ class Counter extends Component {
     textAlign: 'center',
   };
 
-  handleIncrement() {
-      console.log('You pressed a btn');
-      console.log("this.state.count", this);
-      // obj.method() this === obj
-      // function() this === window, undefined
-  }
+  // constructor() {
+  //   super();
+  //   console.log('contructor', this.state);
+  //   // this -> Counter
+  //   // this -> Counter this.handleIncrement viduje
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  // arrow funkcija nekuria savo this
+  handleIncrement = () => {
+    console.log('You pressed a btn');
+    console.log('this.state.count', this.state.count);
+    // obj.method() this === obj
+    // function() this === window, undefined
+  };
 
   render() {
     return (
       <div className="container mt-4">
         <h2 style={{ fontSize: '2em' }}>I am React counter component</h2>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-warning"> Press me</button>
+        <button onClick={this.handleIncrement} className="btn btn-warning">
+          Press me
+        </button>
 
         <ul className="list-group mt-4">
-            {/* Jei kaire salyga true grazinam kas po desine && ... */}
-            {this.state.colors.length === 0 && <p className="alert alert-warning">There are no colors left</p>}
-            {this.renderColors()}
-            </ul>
+          {/* Jei kaire salyga true grazinam kas po desine && ... */}
+          {this.state.colors.length === 0 && (
+            <p className="alert alert-warning">There are no colors left</p>
+          )}
+          {this.renderColors()}
+        </ul>
 
         <div className="mt-5">
           <img src={this.state.imgUrl} alt="tech" />
@@ -41,7 +54,7 @@ class Counter extends Component {
     );
   }
 
- renderColors() {
+  renderColors() {
     return this.state.colors.map((color) => (
       <li key={color} style={{ background: color }} className="list-group-item">
         {color}
