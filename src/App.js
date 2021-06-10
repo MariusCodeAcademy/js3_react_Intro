bdaa5f42a94531307775ede927c910205ff7432e
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-10 10:59:24 +0300

    mv state + hanle fn to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,59 @@ import Counters from './components/counters';
 import Navbar from './components/navBar';
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 10 },
+      { id: 3, value: 1 },
+      { id: 4, value: 11 },
+    ],
+  };
+
+  handleDelete = (idToDelete) => {
+    // gauti state busena be to el kuris buvo paspaustas
+    const countersWihtoutOne = this.state.counters.filter((c) => c.id !== idToDelete);
+    // console.log(countersWihtoutOne);
+    // nustatyti nauja busena su setState()
+    this.setState({ counters: countersWihtoutOne });
+    // console.log('whichOneClicked', idToDelete);
+  };
+
+  handleReset = () => {
+    console.log('Reset please');
+    //gauti busena kuri yra counters kopija kurioje visi value: 0
+    const nunulinta = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    console.log(nunulinta);
+    // gave nustatom busena // patikrinam ar veikia
+    this.setState({ counters: nunulinta });
+  };
+
+  // arrow funkcija nekuria savo this
+  handleIncrement = (plusOrMinus, counter) => {
+    // console.log('plusOrMinus', plusOrMinus);
+    // console.log('counter', counter);
+
+    // pasidaryt state.counters kopija
+    const countersCopy = [...this.state.counters];
+
+    // surasti kuris counteris parase padinti
+    const counterToIncrement = countersCopy.find((c) => c.id === counter.id);
+
+    // kopijoj padidinti value 1
+    counterToIncrement.value++;
+    this.setState({ counters: countersCopy });
+
+    // niekada nekeiciam state tiesiogiai !!!!!!
+    // this.state.count++;
+
+    // let diff = btnId === 'btn_1' ? 1 : -1;
+
+    // this.setState({ value: this.state.value + diff });
+  };
+
   render() {
     return (
       <div className="App ">
