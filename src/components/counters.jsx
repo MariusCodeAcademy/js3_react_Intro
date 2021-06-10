93a21d0e1e79b80b436e002d5818ef5411ca04fa
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-10 11:58:11 +0300

    obj detruct

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,19 +3,16 @@ import Counter from './counter';
 
 class Counters extends Component {
   render() {
+    const { onDelete, onIncrement, onReset, counters } = this.props;
+
     return (
       <div>
         <h2>Counters component</h2>
-        <button onClick={this.props.onReset} className="btn btn-sm btn-warning">
+        <button onClick={onReset} className="btn btn-sm btn-warning">
           Reset counters
         </button>
-        {this.props.counters.map((c) => (
-          <Counter
-            key={c.id}
-            counter={c}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
-          ></Counter>
+        {counters.map((c) => (
+          <Counter key={c.id} counter={c} onIncrement={onIncrement} onDelete={onDelete}></Counter>
         ))}
       </div>
     );
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
-
-export default class Navbar extends Component {
-  render() {
-    const { countersCount, activeCount } = this.props;
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0">
-          <strong>Counter</strong>Anywhere{' '}
-          <span className="badge badge-pill badge-primary">
-            {countersCount}/{activeCount}
-          </span>
+// Functional statless component
+const Navbar = ({ countersCount, activeCount }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand mb-0">
+        <strong>Counter</strong>Anywhere{' '}
+        <span className="badge badge-pill badge-primary">
+          {countersCount}/{activeCount}
         </span>
-      </nav>
-    );
-  }
-}
+      </span>
+    </nav>
+  );
+};
+export default Navbar;
